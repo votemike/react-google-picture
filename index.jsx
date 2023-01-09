a882d5ba1e22baaf29fc2f5702f2fcd6f66913bc
Michael Gwynne <michael@votemike.co.uk>
2023-01-09 15:00:00 +0000

    Using object instead of individual params

--- index.jsx
+++ index.jsx
@@ -1,12 +1,12 @@
 import urlBuilder from "lh3-googleusercontent-url-builder";
 
-const Source = (imageType, imageWidth, url) => (
+const Source = ({imageType, imageWidth, url}) => (
   <source srcSet={`${urlBuilder(url, {
     imageType: imageType,
     width: imageWidth * 2
   })} 2x, ${urlBuilder(url, {imageType: imageType, width: imageWidth * 2})} 1x`} type={`image/${imageType}`}/>
 );
-const GooglePicture = (alt, imageType, imageWidth, url) => (
+const GooglePicture = ({alt, imageType, imageWidth, url}) => (
   <picture>
     <Source url={url} imageType="webp" imageWidth={imageWidth}/>
     <Source url={url} imageType={imageType} imageWidth={imageWidth}/>
