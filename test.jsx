e326e3d338f0e6a752b82a58fa0d21fc17bd323a
Michael Gwynne <michael@votemike.co.uk>
2023-01-12 07:44:00 +0000

    Tidy

--- test.jsx
+++ test.jsx
@@ -1,11 +1,10 @@
-import {logRoles, render} from '@testing-library/react';
+import {render} from '@testing-library/react';
 import GooglePicture from "./index";
 
 it('renders a webp source with a 1x and a 2x pixel density, a jpeg source with a 1x and a 2x pixel density and an img inside a picture tag', () => {
   const {container} = render(
     <GooglePicture alt="The alt" imageFormat="jpeg" imageWidth={600} url="https://lh3.googleusercontent.com/ABC123etc"/>
   );
-  logRoles(container)
   expect(container).toMatchSnapshot();
 });
 
