ede1c5071eea40cea3bf73ed14797a01cfc9b94b
Michael Gwynne <michael@votemike.co.uk>
2023-01-12 07:42:41 +0000

    Multiple screen sizes

--- test.jsx
+++ test.jsx
@@ -1,17 +1,24 @@
-import {logRoles, render} from '@testing-library/react';
+import {render} from '@testing-library/react';
 import GooglePicture from "./index";
 
 it('renders a webp source with a 1x and a 2x pixel density, a jpeg source with a 1x and a 2x pixel density and an img inside a picture tag', () => {
   const {container} = render(
-    <GooglePicture alt="The alt" imageFormat="jpeg" imageWidth={600} url="https://lh3.googleusercontent.com/ABC123etc"/>
+    <GooglePicture alt="The alt" imageFormat="jpeg" widths={{defaultWidth: 200}} url="https://lh3.googleusercontent.com/ABC123etc"/>
   );
-  logRoles(container)
   expect(container).toMatchSnapshot();
 });
 
 it('renders a webp source with a 1x and a 2x pixel density, a png source with a 1x and a 2x pixel density and an img inside a picture tag', () => {
   const {container} = render(
-    <GooglePicture alt="The alt" imageFormat="png" imageWidth={600} url="https://lh3.googleusercontent.com/ABC123etc"/>
+    <GooglePicture alt="The alt" imageFormat="png" widths={{defaultWidth: 300}} url="https://lh3.googleusercontent.com/ABC123etc"/>
+  );
+  expect(container).toMatchSnapshot();
+});
+
+it('renders a 480px wide image on screens 600px and smaller and renders a 600px image on any device larger', () => {
+  const {container} = render(
+    <GooglePicture alt="The alt" imageFormat="png" widths={{defaultWidth: 600, 600: 480}}
+                   url="https://lh3.googleusercontent.com/ABC123etc"/>
   );
   expect(container).toMatchSnapshot();
 });
